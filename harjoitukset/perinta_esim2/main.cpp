43bc0f7e030620cf281a76babea9c7d8859e8647
Jani <78798020+Jaxi84@users.noreply.github.com>
2025-11-18 17:39:07 +0200

    perinta esim2

---
+++ harjoitukset/perinta_esim2/main.cpp
@@ -0,0 +1,41 @@
+#include "pesukone.h"
+#include "televisio.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    Tuote objTuote("Sohva",999.99);
+    Televisio objTelevisio("TV",850,55);
+    Pesukone objPesukone("Pyykkikone",340.49, 1400, 10);
+    objTuote.tulostaTiedot();
+    objTelevisio.tulostaTiedot();
+    objPesukone.tulostaTiedot();
+
+    vector<Televisio> tvLista;
+    tvLista.emplace_back("LG TV",999,75);
+    tvLista.emplace_back("Samsung TV",1999,88);
+    tvLista.emplace_back("Philips TV",699,60);
+
+    cout<<"Televisio lista:"<<endl;
+    for(Televisio tv: tvLista){
+        tv.tulostaTiedot();
+    }
+
+    vector<Tuote*> tuoteLista;
+    tuoteLista.push_back(&objTuote);
+    tuoteLista.push_back(&objPesukone);
+    tuoteLista.push_back(&objTelevisio);
+
+    cout<<"Tuote lista:"<<endl;
+    for(auto tuote: tuoteLista){
+        tuote->tulostaTiedot();
+    }
+    //auto sanan ansiosta kääntäjä tunnistaa olion luokan
+    //emplace_back luodana olio samantien
+    //push_back jos olio on luotu etukäteen
+
+    return 0;
+}
